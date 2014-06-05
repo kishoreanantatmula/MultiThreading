f5e77363860b55f293f7324e92584af118248b35
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 18:06:56 +0530

    Commit
    Comit

---
+++ Threads/src/demo2.java
@@ -0,0 +1,15 @@
+
+public class demo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("THi is Demo 2");
+		
+		
+	}
+
+}
