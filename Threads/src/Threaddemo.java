8fa313abdcfae76a9298e14507ff4e3bf75239f3
kishoreanantatmula <anantha6445@gmail.com>
2014-06-05 16:06:40 +0530

    commiting new Prgram
    New Prg

---
+++ Threads/src/Threaddemo.java
@@ -0,0 +1,51 @@
+
+public class Threaddemo extends Thread {
+	
+	
+	public void run()
+	{
+		
+		for(int i = 0;i<5;i++)
+		{
+			
+			try {
+				Thread.sleep(1000);
+				System.out.println("Child Thread");
+			} catch (InterruptedException e) {
+					e.printStackTrace();
+			}
+			
+			
+		}
+		
+				
+		
+	}
+	
+	
+	public static void main(String args[])
+	{
+		
+		Threaddemo t  = new Threaddemo();
+		
+		t.start();
+		
+		for(int i = 0;i<5;i++)
+		{
+			
+			try {
+				Thread.sleep(1000);
+				System.out.println("Main Thread");
+			} catch (InterruptedException e) {
+					e.printStackTrace();
+			}
+			
+		
+		 
+	}
+
+	
+	
+	
+	}
+}
